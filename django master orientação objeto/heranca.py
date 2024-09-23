07b2edf02eb1b55d3e29f14f3ff983e13778e889
alexsandro <alexsandrodummer>
2024-09-23 10:22:01 -0300

    primeiro commit

---
+++ django master orientação objeto/heranca.py
@@ -0,0 +1,21 @@
+class carro:
+    numero_rodas = 4
+    quantidade_passageiros = 5
+
+    def acelerar (self):
+        print ('acelerando...')
+
+    def frear(self):
+        print ('freando...')
+
+        def buzinar(self):
+            print ('buzinando...')
+
+
+class Uno(carro):
+    modelo = 'Uno'
+    marca = 'Fiat'
+    ano = 2015
+
+uno = Uno()
+uno.frear()
