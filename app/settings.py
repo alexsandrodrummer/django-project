d7641c843b721cd264515655c76771cbca9d27c2
Alex Sandro <79149058+alexsandrodrummer@users.noreply.github.com>
2024-11-27 10:12:20 -0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'carros',
         'USER': 'postgres',
-        'PASSWORD': '@#$alexx',
+        'PASSWORD': '...',
         'HOST': 'localhost',
         'PORT': '5432',
     }
